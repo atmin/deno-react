dc784a1620d7e97e569408033430c19857719b8f
Atanas Minev <atmin@protonmail.com>
2021-02-23 00:15:29 +0200

    add vscode settings, utility scripts

---
+++ dev.sh
@@ -0,0 +1,22 @@
+intexit() {
+    # Kill all subprocesses (all processes in the current process group)
+    kill -HUP -$$
+}
+
+hupexit() {
+    # HUP'd (probably by intexit)
+    echo
+    echo "Interrupted"
+    exit
+}
+
+trap hupexit HUP
+trap intexit INT
+
+# Browser bundle
+deno bundle --watch --importmap=import_map.json --unstable src/browser.tsx public/app.js &
+
+# Server
+deno run --watch --importmap=import_map.json --unstable --allow-net --allow-read src/server.tsx &
+
+wait
