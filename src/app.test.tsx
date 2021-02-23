e499c26765cc3c92c3618ae5a34c63a242fca10c
Atanas Minev <atmin@protonmail.com>
2021-02-23 21:02:33 +0200

    fix .gitignore'd x/gen/file

--- src/app.test.tsx
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "std/testing/asserts.ts";
-
-Deno.test("1 + 1 = 2", () => {
-  const x = 1 + 1;
-  assertEquals(x, 2);
-});
--- x/gen/file/Users/atmin/dev/deno-react/src/browser.tsx.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.tsx";
-ReactDOM.hydrate(React.createElement(App, null), document.getElementById("root"));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZmlsZTovLy9Vc2Vycy9hdG1pbi9kZXYvZGVuby1yZWFjdC9zcmMvYnJvd3Nlci50c3giXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxLQUFLLE1BQU0sT0FBTyxDQUFDO0FBQzFCLE9BQU8sUUFBUSxNQUFNLFdBQVcsQ0FBQztBQUNqQyxPQUFPLEdBQUcsTUFBTSxXQUFXLENBQUM7QUFFM0IsUUFBZ0IsQ0FBQyxPQUFPLENBQ3ZCLG9CQUFDLEdBQUcsT0FBRyxFQUVQLFFBQVEsQ0FBQyxjQUFjLENBQUMsTUFBTSxDQUFDLENBQ2hDLENBQUMifQ==
